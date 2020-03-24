d79593602761bcc8d487c41925b5f0b6f9aefe9a
msaarna@gmail.com <msaarna@gmail.com>
2020-03-23 20:10:29 -0400

    correct amiga mouse buttons

--- src/devices/bus/a7800_ctrl/amigamouse.cpp
+++ src/devices/bus/a7800_ctrl/amigamouse.cpp
@@ -17,15 +17,12 @@
 
 DEFINE_DEVICE_TYPE(A7800_AMIGAMOUSE, a7800_amigamouse_device, "a7800_amigamouse", "Amiga mouse")
 
-	// Only 1 button is presently supported. It's not clear at this time if the Amiga
-	// mouse has pull-ups on the second and third buttons, which would be required for
-	// them to be reliably used with the 7800 paddle lines.
 
 static INPUT_PORTS_START( a7800_amigamouse )
 	PORT_START("JOY")
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON1 ) //pin 6
-	PORT_BIT( 0x5F, IP_ACTIVE_HIGH, IPT_UNUSED  )
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_BUTTON1 )
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON2 )
+	PORT_BIT( 0x9F, IP_ACTIVE_HIGH, IPT_UNUSED  )
 
 	PORT_START("MOUSEX")
 	PORT_BIT( 0xff, 0x00, IPT_TRACKBALL_X ) PORT_SENSITIVITY(40) PORT_KEYDELTA(5)
