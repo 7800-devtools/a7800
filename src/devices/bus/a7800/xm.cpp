bb607b59788e7a1126f482959fbb773e4755f0c7
msaarna@gmail.com <msaarna@gmail.com>
2020-05-23 19:44:21 -0400

    first pass: 48k flat ram, ram WE flag

--- src/devices/bus/a7800/xm.cpp
+++ src/devices/bus/a7800/xm.cpp
@@ -114,23 +114,35 @@ MACHINE_CONFIG_END
 
 READ8_MEMBER(a78_xm_device::read_40xx)
 {
-        if (BIT(m_cntrl1,5) && offset < 0x2000)
-                return m_ram[ ((offset&0x1fff) + (((m_cntrl2&15) * 0x2000))) | ((m_cntrl1&1)<<8) ];
-        else if ( BIT(m_cntrl1,6) && offset >= 0x2000 && offset < 0x4000)
-                return m_ram[ ((offset&0x1fff) + ((((m_cntrl2>>4)&15) * 0x2000))) | ((m_cntrl1&2)<<7) ];
-        else
-                return m_xmslot->read_40xx(space, offset);
+	if (!BIT(m_cntrl5,0))
+        {
+		if (BIT(m_cntrl1,5) && offset < 0x2000)
+			return m_ram[ ((offset&0x1fff) + (((m_cntrl2&15) * 0x2000))) | ((m_cntrl1&1)<<8) ];
+		else if ( BIT(m_cntrl1,6) && offset >= 0x2000 && offset < 0x4000)
+			return m_ram[ ((offset&0x1fff) + ((((m_cntrl2>>4)&15) * 0x2000))) | ((m_cntrl1&2)<<7) ];
+		else
+			return m_xmslot->read_40xx(space, offset);
+        }
+	else
+		return m_ram[ offset ];
 }
 
 WRITE8_MEMBER(a78_xm_device::write_40xx)
 {
-        if (BIT(m_cntrl1, 5) && offset < 0x2000)
-                m_ram[ (offset) + (((m_cntrl2&15) * 0x2000)) ] = data;
-        else if (BIT(m_cntrl1,6) && offset >= 0x2000 && offset < 0x4000)
-                 m_ram[ offset + ((((m_cntrl2>>4)&15) * 0x2000)) ] = data;
-        else
-                m_xmslot->write_40xx(space, offset, data);
-	// TODO: implement ROF bits in cntrl1
+	if (!BIT(m_cntrl5,1)) // only write if XM !WE is flagged
+	{
+		if (!BIT(m_cntrl5,0))
+        	{
+			if (BIT(m_cntrl1, 5) && offset < 0x2000)
+				m_ram[ (offset) + (((m_cntrl2&15) * 0x2000)) ] = data;
+			else if (BIT(m_cntrl1,6) && offset >= 0x2000 && offset < 0x4000)
+				m_ram[ offset + ((((m_cntrl2>>4)&15) * 0x2000)) ] = data;
+			else
+				m_xmslot->write_40xx(space, offset, data);
+		}
+		else // flat 48k RAM mode is active
+			m_ram[ offset ] = data;
+	}
 }
 
 
